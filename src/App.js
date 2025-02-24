1129c114f3d8f19f3a4157085aa5dc079219e13c
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-23 22:03:01 -0500

    add Header component and update styling for improved layout and navigation

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import HomePage from "./pages/HomePage";
 import HillmanPage from "./pages/HillmanPage";
 import OtherPage from "./pages/OtherPage";
 import ErrorPage from "./pages/ErrorPage";
+import Header from "./components/Header";
+
 
 function App() {
-  return <div className="App">Hello Building managment!
+  return <div className="App">
+    <Header/>
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="hillman" element={<HillmanPage />} />
